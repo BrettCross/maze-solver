be930180e680c30ebebff72cdb054821075fe7f5
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2025-05-15 10:42:12 -0400

    refactor code to add window class in own module

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-from tkinter import Tk, BOTH, Canvas
-
-class Window:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.root = Tk()
-        self.root.title("Maze Solver")
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.is_window_running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-
-    def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
-    
-    def wait_for_close(self):
-        self.is_window_running = True
-        while self.is_window_running:
-            self.redraw()
-
-    def close(self):
-        self.is_window_running = False
+from window import Window
 
 def main():
     win = Window(800, 600)
     win.wait_for_close()
 
-main()
+
+if __name__ == "__main__":
+    main()
---
+++ window.py
@@ -0,0 +1,26 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.root.title("Maze Solver")
+
+        self.canvas = Canvas(self.root, bg="white")
+        self.canvas.pack()
+        self.is_window_running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+    
+    def wait_for_close(self):
+        self.is_window_running = True
+        while self.is_window_running:
+            self.redraw()
+
+    def close(self):
+        self.is_window_running = False
