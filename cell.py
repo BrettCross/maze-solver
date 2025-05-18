ef898611ac9e6b90dafceecd4ae083def6111177
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2025-05-17 22:40:56 -0400

    add draw_move function that will highlight correct path through maze

--- cell.py
+++ cell.py
@@ -39,3 +39,20 @@ class Cell:
             point2 = Point(x2, y2)
             line = Line(point1, point2)
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        fill = "red"
+        if undo:
+            fill= "gray"
+
+        # from_cell [x=(x1+x2)/2, y=(y1+y2)/2] to_cell [x=(x1+x2)/2, y=(y1+y2)/2]
+        x = (self._x1 + self._x2)/2
+        y = (self._y1 + self._y2)/2
+        point1 = Point(x,y)
+
+        t_x = (to_cell._x1 + to_cell._x2)/2
+        t_y = (to_cell._y1 + to_cell._y2)/2
+        point2 = Point(t_x, t_y)
+
+        line = Line(point1, point2)
+        self._win.draw_line(line, fill_color=fill)
--- main.py
+++ main.py
@@ -7,22 +7,25 @@ from point import Point
 def main():
     win = Window(800, 600)
     
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(50, 50, 100, 100)
+    a = Cell(win)
+    a.has_left_wall = False
+    a.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(125, 125, 200, 200)
+    b = Cell(win)
+    b.has_right_wall = False
+    b.draw(125, 50, 175, 100)
 
     c = Cell(win)
     c.has_bottom_wall = False
-    c.draw(225, 225, 250, 250)
+    c.draw(200, 50, 250, 100)
 
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(300, 300, 500, 500)
+    d = Cell(win)
+    d.has_top_wall = False
+    d.draw(200, 125, 250, 175)
     
+    a.draw_move(b)
+    b.draw_move(c, undo=True)
+    b.draw_move(d)
     win.wait_for_close()
 
 
