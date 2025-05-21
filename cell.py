e5eec2e371a635106d5bb464a92490f88f69109e
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2025-05-20 22:09:30 -0400

    add unit tests

--- cell.py
+++ cell.py
@@ -3,42 +3,44 @@ from line import Line
 
 
 class Cell:
-    def __init__(self, win):
-        self._win = win
-        self._x1 = -1
-        self._x2 = -1
-        self._y1 = -1
-        self._y2 = -1
+    def __init__(self, win=None):
+        self.__win = win
+        self.__x1 = -1
+        self.__x2 = -1
+        self.__y1 = -1
+        self.__y2 = -1
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
 
     def draw(self, x1, y1, x2, y2):
-        self._x1 = x1
-        self._y1 = y1
-        self._x2 = x2
-        self._y2 = y2
+        if self.__win is None:
+            return
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
         if self.has_left_wall:
             point1 = Point(x1, y1)
             point2 = Point(x1, y2)
             line = Line(point1, point2)
-            self._win.draw_line(line)
+            self.__win.draw_line(line)
         if self.has_top_wall:
             point1 = Point(x1, y1)
             point2 = Point(x2, y1)
             line = Line(point1, point2)
-            self._win.draw_line(line)
+            self.__win.draw_line(line)
         if self.has_right_wall:
             point1 = Point(x2, y1)
             point2 = Point(x2, y2)
             line = Line(point1, point2)
-            self._win.draw_line(line)
+            self.__win.draw_line(line)
         if self.has_bottom_wall:
             point1 = Point(x1, y2)
             point2 = Point(x2, y2)
             line = Line(point1, point2)
-            self._win.draw_line(line)
+            self.__win.draw_line(line)
 
     def draw_move(self, to_cell, undo=False):
         fill = "red"
@@ -55,4 +57,4 @@ class Cell:
         point2 = Point(t_x, t_y)
 
         line = Line(point1, point2)
-        self._win.draw_line(line, fill_color=fill)
+        self.__win.draw_line(line, fill_color=fill)
