66369ba8c7c9c1ecf61313f5b0e4d7a2eafec4db
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2025-05-21 22:18:03 -0400

    add DFS solve traversal

--- cell.py
+++ cell.py
@@ -54,12 +54,12 @@ class Cell:
             fill= "gray"
 
         # from_cell [x=(x1+x2)/2, y=(y1+y2)/2] to_cell [x=(x1+x2)/2, y=(y1+y2)/2]
-        x = (self._x1 + self._x2)/2
-        y = (self._y1 + self._y2)/2
+        x = (self.__x1 + self.__x2)/2
+        y = (self.__y1 + self.__y2)/2
         point1 = Point(x, y)
 
-        t_x = (to_cell._x1 + to_cell._x2)/2
-        t_y = (to_cell._y1 + to_cell._y2)/2
+        t_x = (to_cell.__x1 + to_cell.__x2)/2
+        t_y = (to_cell.__y1 + to_cell.__y2)/2
         point2 = Point(t_x, t_y)
 
         line = Line(point1, point2)
